a4128fb1002f349d6fa15d15915becf1169f6d9b
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2024-09-24 02:38:09 +0600

    history section added

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 
+console.log(new Date().toString())
+// Tue Sep 24 2024 01:48:06 GMT+0600 (Bangladesh Standard Time)
+
+
 document.getElementById('noakhali-donate').addEventListener('click', function () {
     const inputValue = getInputValue('input-noakhali');
 
@@ -22,8 +26,8 @@ document.getElementById('noakhali-donate').addEventListener('click', function ()
         document.getElementById('my_modal_1').showModal();
     }
 
-    document.getElementById('balance-noakhali').innerText = addDonation;
-    document.getElementById('main-balance').innerText = leftBalance;
+    document.getElementById('balance-noakhali').innerText = addDonation.toFixed(2);
+    document.getElementById('main-balance').innerText = leftBalance.toFixed(2);
     document.getElementById('input-noakhali').value = '';
 
 })
@@ -51,8 +55,8 @@ document.getElementById('feni-donate').addEventListener('click', function () {
         document.getElementById('my_modal_1').showModal();
     }
 
-    document.getElementById('balance-feni').innerText = addDonation;
-    document.getElementById('main-balance').innerText = leftBalance;
+    document.getElementById('balance-feni').innerText = addDonation.toFixed(2);
+    document.getElementById('main-balance').innerText = leftBalance.toFixed(2);
     document.getElementById('input-feni').value = ' ';
 
 
@@ -81,8 +85,40 @@ document.getElementById('quota-donate').addEventListener('click', function () {
         document.getElementById('my_modal_1').showModal();
     }
 
-    document.getElementById('balance-quota').innerText = addDonation;
-    document.getElementById('main-balance').innerText = leftBalance;
+    document.getElementById('balance-quota').innerText = addDonation.toFixed(2);
+    document.getElementById('main-balance').innerText = leftBalance.toFixed(2);
     document.getElementById('input-quota').value = '';
 
+})
+
+
+// history tab
+
+const historyTab = document.getElementById('history-tab');
+const donationTab = document.getElementById('donation-tab');
+const donationSection = document.getElementById('donation-section')
+const historySection = document.getElementById('history-section')
+
+historyTab.addEventListener('click', function () {
+    historyTab.classList.add('btn-color');
+    historyTab.classList.remove('btn-outline');
+
+    donationTab.classList.remove('btn-color');
+    donationTab.classList.add('btn-outline');
+
+    donationSection.classList.add('hidden');
+    historySection.classList.remove('hidden');
+    
+})
+
+donationTab.addEventListener('click', function () {
+    donationTab.classList.add('btn-color');
+    donationTab.classList.remove('btn-outline');
+
+    historyTab.classList.remove('btn-color');
+    historyTab.classList.add('btn-outline');
+
+    historySection.classList.add('hidden');
+    donationSection.classList.remove('hidden');
+    
 })
